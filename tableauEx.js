3cf7fa8e9d6c4e649901a07c7f9a3a23df91a3fa
HajliHiba <hajlihiba2@gmail.com>
2026-09-22 18:21:13 +0100

    Add files via upload

---
+++ tableauEx.js
@@ -0,0 +1,8 @@
+let etudiants = ["ahmed","iness","sara","zied","taha"]
+
+for(let i=0 ; i < etudiants.length ; i++){
+    
+        if(etudiants[i].includes("s"))
+            console.log(etudiants[i])
+
+ }
